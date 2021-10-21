ed0126371593e1ccb78a31db641427ab37c8e2dd
Kun Wang <kun.w@hotmail.com>
2021-10-21 22:54:11 +1300

    feat: to add http tests

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 import R from 'ramda';
-import appsetings from '../../../appsetings/appsettings.json';
+import appsetings from '../../../appsettings/appsettings.json';
 import secrets from '../../../secrets/secrets.json';
 
 export const configValues = R.mergeDeepRight(appsetings, secrets);
