3191d87f4bc3d10316b2b5ebef54cacbc9d221dc
Kun Wang <kun.w@hotmail.com>
2021-11-09 10:20:56 +1300

    fix deployment scripts

--- src/modules/ethereum/interface/index.ts
+++ src/modules/ethereum/interface/index.ts
@@ -2,6 +2,7 @@ export enum EthereumNetworkType {
   Mainnet = 'mainnet',
   Rinkeby = 'rinkeby',
   AWS = 'aws',
+  Ropsten = 'ropsten',
 }
 
 export type NetworkType = keyof typeof EthereumNetworkType;
