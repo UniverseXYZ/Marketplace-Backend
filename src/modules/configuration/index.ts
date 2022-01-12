94db90521eca16764bd3927d6965419cfa03376c
Kun Wang <kun.w@hotmail.com>
2022-01-12 22:31:54 +1300

    improve the support for process.env (#14)

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -1,14 +1,6 @@
 import { registerAs } from '@nestjs/config';
-import R from 'ramda';
-import { getAppSettings, getSecrets } from '../../utils/config';
 
-const appsettings = getAppSettings('appsettings');
-const secrets = getSecrets('secrets');
-
-export const configValues = R.mergeDeepRight(
-  R.mergeDeepRight(appsettings, secrets),
-  process.env,
-);
+export const configValues = process.env;
 
 export default registerAs('config', () => {
   return configValues;
