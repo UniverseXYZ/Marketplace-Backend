211885ebc514ac88e77fa980f220c3c4ebd8e212
Kun Wang <kun.w@hotmail.com>
2022-01-12 17:19:35 +1300

    add support for process env

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -5,7 +5,10 @@ import { getAppSettings, getSecrets } from '../../utils/config';
 const appsettings = getAppSettings('appsettings');
 const secrets = getSecrets('secrets');
 
-export const configValues = R.mergeDeepRight(appsettings, secrets);
+export const configValues = R.mergeDeepRight(
+  R.mergeDeepRight(appsettings, secrets),
+  process.env,
+);
 
 export default registerAs('config', () => {
   return configValues;
