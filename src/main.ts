c36d565ed28580bff112d6082042d45e482fa300
Kun Wang <kun.w@hotmail.com>
2022-01-14 09:42:28 +1300

    make swagger public

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('doc', app, document);
+  SwaggerModule.setup('v1/doc', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
