a77cf0981ed58ef026a75072d70b778c0bbca956
Paul <33381806+PavelDonskikh@users.noreply.github.com>
2022-03-20 16:52:41 -0400

    sc-2324: Exclude ERC20 asset from calculating the volume traded (#58)
    * sc-2324: Exclude ERC20 asset from calculating the volume traded
    
    * sc-2324: Exclude ERC20 asset from calculating the volume traded

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1243,17 +1243,19 @@ export class OrdersService {
       .createQueryBuilder('o')
       .where(
         `
-        o.side = :side AND
         o.status = :status AND
-        LOWER(o.take->'assetType'->>'contract') = :contract
+        o.side = :side AND 
+        LOWER(o.make->'assetType'->>'contract') = :contract AND
+        o.take->'assetType'->>'assetClass' = :assetClass
       `,
         {
-          side: OrderSide.BUY,
           status: OrderStatus.FILLED,
+          side: OrderSide.SELL,
           contract: collection.toLowerCase(),
+          assetClass: AssetClass.ETH,
         },
       )
-      .select(`SUM(CAST(o.make->>'value' as DECIMAL))`, 'volumeTraded')
+      .select(`SUM(CAST(o.take->>'value' as DECIMAL))`, 'volumeTraded')
       .getRawOne();
     if (orders.volumeTraded) {
       value = orders.volumeTraded;
