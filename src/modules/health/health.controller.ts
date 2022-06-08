a0b2861e807027c654c0b73f11b6765da2405bc9
Boris Bonin <boris.bonin@limechain.tech>
2022-06-08 15:53:57 +0300

    Remove db healthcheck

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,20 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import {
-  HealthCheckService,
-  HealthCheck,
-  TypeOrmHealthIndicator,
-} from '@nestjs/terminus';
+import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 import { BaseController } from '../../common/base.controller';
 import { EthHealthIndicator } from '../eth-health/eth-health.service';
 
 @Controller('health')
 @ApiTags('Health')
 export class HealthController extends BaseController {
-  
   constructor(
     private health: HealthCheckService,
-    private db: TypeOrmHealthIndicator,
     private eth: EthHealthIndicator,
   ) {
     super(HealthController.name);
@@ -24,11 +18,8 @@ export class HealthController extends BaseController {
   @HealthCheck()
   async healthCheck() {
     try {
-      return this.health.check([
-        () => this.db.pingCheck('database'),
-        () => this.eth.pingCheck('ethereum'),
-      ]);
-    } catch(e) {
+      return this.health.check([() => this.eth.pingCheck('ethereum')]);
+    } catch (e) {
       this.logger.error(e);
       this.errorResponse(e);
     }
