ddf69f77bfa87194d96ae262a6896cad818fecf7
vikinatora <vikinatora@gmail.com>
2022-06-21 17:53:45 +0300

    try fix mongodb connection issue on dev

--- src/modules/mongo-database/mongo-database.module.ts
+++ src/modules/mongo-database/mongo-database.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppConfigModule } from '../configuration/configuration.module';
 import { MongoDatabaseService } from './mongo-database.service';
 
 @Module({
+  imports: [AppConfigModule],
   providers: [MongoDatabaseService],
   exports: [MongoDatabaseService],
 })
--- src/modules/mongo-database/mongo-database.service.ts
+++ src/modules/mongo-database/mongo-database.service.ts
@@ -5,13 +5,13 @@ import {
 } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 import uniqueValidator from 'mongoose-unique-validator';
-import { ConfigService } from '@nestjs/config';
+import { AppConfig } from '../configuration/configuration.service';
 
 @Injectable()
 export class MongoDatabaseService implements MongooseOptionsFactory {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: AppConfig) {}
   createMongooseOptions(): MongooseModuleOptions {
-    const uri: string = this.configService.get('MONGODB_URI');
+    const uri: string = this.configService.values.MONGODB_URI;
     if (!uri) {
       throw new Error('MongoDB URI is not defined');
     }
